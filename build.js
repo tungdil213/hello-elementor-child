242801c28bb4c7b5776358785e5f5a5962fbac6e
Tungdil213 <admin@herric.de>
2022-07-22 11:40:27 +0200

    audit fix, add babel, add type module

--- build.js
+++ build.js
@@ -1,12 +1,14 @@
-const esbuild = require('esbuild');
-const sassPlugin = require('esbuild-plugin-sass')
+import esbuild from 'esbuild';
+import babel from 'esbuild-plugin-babel';
+import sassPlugin from 'esbuild-plugin-sass';
 
 console.log("build scripts files")
 esbuild.build({
     entryPoints: ['assets/scripts/app.js'],
     bundle: true,
     minify: true,
-    outfile: 'dist/app.js'
+    outfile: 'dist/app.js',
+    plugins: [babel()]
 })
 .catch((e) => console.error(e.message))
 
